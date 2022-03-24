60eb4a6bc8d0583251e1fe6ab2f9f10b38871934
Wellinton <wellinton.s1994@aluno.ifsc.edu.br>
2022-03-24 20:26:35 -0300

    Update messages

--- aurup.sh
+++ aurup.sh
@@ -4,7 +4,7 @@
 cd /tmp/
 option="$1"
 package="$2"
-version="1.0.0-alpha10"
+version="1.0.0-alpha11"
 
 red=`tput setaf 1`
 green=`tput setaf 2`
@@ -36,13 +36,11 @@ function printVersion {
 function searchPackage {
 	url="https://aur.archlinux.org/packages/?O=0&SeB=nd&K=$package&outdated=&SB=n&SO=a&PP=100&do_Search=Go"
 	w3m -dump $url | sed -n "/^$package/p" & 
-	echo "listing similar packages to search: $package" 
+	echo "listing similar packages..." 
 	wait
 }
 
 function installPackage {
-	echo "${red}$package ${reset}will be updated"
-	sudo mount -o remount,size=10G /tmp
 	wget -q $url
 	tar -xzf "$package.tar.gz"
 	cd "$package"
@@ -62,33 +60,38 @@ function verifyPackageVersion {
 }
 
 function updatePackages {
-	file1="/tmp/aurup.txt"
-	file2="/tmp/packages.txt"
-	sudo pacman -Qm > $file1
+	allPackages="/tmp/allPackages.txt"
+	outdatedPackages="/tmp/outdatedPackages.txt"
+	
+	sudo pacman -Qm > $allPackages
 	echo "updating the database, please wait..."
+
 	while read -r line; do
 		package="$( echo "$line" | cut -d' ' -f1 )"
 		if verifyPackageVersion; then
 			echo "${green}$package ${reset}is on the latest version"
 		else
 			echo "${red}$package ${reset}needs to be updated"
-			echo "$package" >> $file2
+			echo "$package" >> $outdatedPackages
 		fi
-	done < $file1
+	done < $allPackages
 
-	if [ -s "$file2" ]; then
+	if [ -s "$outdatedPackages" ]; then
+		sudo mount -o remount,size=10G /tmp
 		while read -r line; do
 			package=line
 			url="https://aur.archlinux.org/cgit/aur.git/snapshot/$package.tar.gz"
+			echo "${red}$package ${reset}will be updated"
 			installPackage
-		done < $file2
+		done < $outdatedPackages
 		sudo pacman -Rns $(pacman -Qtdq) --noconfirm
 	else
 		echo ""
 		echo "there are no packages to update"
 	fi
-	sudo rm -rf "$file1"
-	sudo rm -rf "$file2"
+
+	sudo rm -rf "$allPackages"
+	sudo rm -rf "$outdatedPackages"
 }
 
 function verifyDependency {
@@ -98,11 +101,9 @@ function verifyDependency {
 	fi
 }
 
-if [[ "$option" == "--list" || "$option" == "-L" ]];
-then
+if [[ "$option" == "--list" || "$option" == "-L" ]]; then
 	sudo pacman -Qm
-elif [[ "$option" == "--sync" || "$option" == "-S" ]];
-then
+elif [[ "$option" == "--sync" || "$option" == "-S" ]]; then
 	url="https://aur.archlinux.org/cgit/aur.git/snapshot/$package.tar.gz"
 	requestCode="$( curl -Is "$url" | head -1 )"
 	if [[ "$package" == "" || "$package" == " " ]]; then
@@ -112,36 +113,33 @@ then
 		if verifyPackageVersion; then
 			echo "${green}$package ${reset}is in the latest version"
 		else
+			sudo mount -o remount,size=10G /tmp
+			echo "${green}$package ${reset}will be installed"
 			installPackage
 		fi
 	else
 		echo "package does not exist in aur repository"
 	fi	
 
-elif [[ "$option" == "--update" || "$option" == "-Sy" ]]; 
-then
+elif [[ "$option" == "--update" || "$option" == "-Sy" ]]; then
 	verifyDependency
 	updatePackages
-elif [[ "$option" == "--remove" || "$option" == "-R" ]]; 
-then
+elif [[ "$option" == "--remove" || "$option" == "-R" ]]; then
 	if [[ "$package" == "" || "$package" == " " ]]; then
 		printError
 	else
 		sudo pacman -R "$package"
 	fi
-elif [[ "$option" == "--help" || "$option" == "-h" ]]; 
-then
+elif [[ "$option" == "--help" || "$option" == "-h" ]]; then
 	printManual
-elif [[ "$option" == "--search" || "$option" == "-Ss" ]];
-then
+elif [[ "$option" == "--search" || "$option" == "-Ss" ]]; then
 	if [[ "$package" == "" || "$package" == " " ]]; then
 		printError
 	else
 		verifyDependency
 		searchPackage
 	fi
-elif [[ "$option" == "--version" || "$option" == "-V" ]]; 
-then
+elif [[ "$option" == "--version" || "$option" == "-V" ]]; then
 	printVersion
 else
 	printError
